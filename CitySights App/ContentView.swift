dbe7c5f4f5f1f552747dae28aa36667ddf2b8e23
Chris Ching <chris@codewithchris.com>
2023-09-12 15:04:28 -0400

    - implement query textfield and go button

--- CitySights App/ContentView.swift
+++ CitySights App/ContentView.swift
@@ -8,17 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var query: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, stranger!")
-            
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, stranger!")
+        HStack {
+            TextField("What're you looking for?", text: $query)
+                .textFieldStyle(.roundedBorder)
+            Button {
+                // TODO: Implement query
+            } label: {
+                Text("Go")
+            }
+
         }
         .padding()
     }
