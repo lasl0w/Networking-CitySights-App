0e031d7dcb562061ebb9cb2a9be21fcf0b08f9a1
Chris Ching <chris@codewithchris.com>
2023-09-12 14:13:17 -0400

    Initial Commit

---
+++ CitySights App/CitySights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySights_AppApp.swift
+//  CitySights App
+//
+//  Created by Chris Ching on 2023-09-12.
+//
+
+import SwiftUI
+
+@main
+struct CitySights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CitySights App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CitySights App
+//
+//  Created by Chris Ching on 2023-09-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
